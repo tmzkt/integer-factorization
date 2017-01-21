91889c7c5842b8ec09586063c8d5bca7032cbcb8
Scott <ssk_8189@yahoo.com>
2017-01-20 21:58:03 -0500

    add primes class to generate prime numbers

---
+++ src/main/java/com/tmzkt/prime/Primes.java
@@ -0,0 +1,16 @@
+package com.tmzkt.prime;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Primes {
+    public static List<BigInteger> primeSieve(BigInteger n) {
+        List<BigInteger> primes = new ArrayList<>();
+        if (n.compareTo(BigInteger.ONE) <= 0) {
+            return primes;
+        }
+
+        return primes;
+    }
+}
