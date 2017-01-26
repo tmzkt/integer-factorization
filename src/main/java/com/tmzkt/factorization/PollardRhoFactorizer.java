0c423726157bb8c58232024ba9c54ff6392f1f29
Scott <ssk_8189@yahoo.com>
2017-01-25 22:18:51 -0500

    add outline of pollard rho algorithm

--- src/main/java/com/tmzkt/factorization/PollardRhoFactorizer.java
+++ src/main/java/com/tmzkt/factorization/PollardRhoFactorizer.java
@@ -11,7 +11,25 @@ public class PollardRhoFactorizer implements Factorizer {
         if (n.compareTo(BigInteger.valueOf(2)) < 0) {
             return primeFactors;
         }
+        // TODO check if number is prime?
+
+        // TODO test this code and fix it :)
+        BigInteger x = BigInteger.valueOf(2), y = BigInteger.valueOf(2), d = BigInteger.ONE;
+        while (d.compareTo(BigInteger.ONE) == 0) {
+            x = g(x);
+            y = g(g(y));
+            d = n.gcd(x.subtract(y).abs());
+            if (d.compareTo(n) == 0) {
+                return primeFactors;
+            } else {
+                primeFactors.add(d);
+            }
+        }
 
         return primeFactors;
+    }
+
+    private static BigInteger g(BigInteger x) {
+        return x.multiply(x).add(BigInteger.ONE);
     }
 }
