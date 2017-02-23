4ffe9f2201197add63303fe28cdf1020535315cf
Scott <ssk_8189@yahoo.com>
2017-02-22 19:43:56 -0500

    divisibility rule for 3 tests

--- src/test/java/com/tmzkt/factorization/DivisibilityRuleTest.java
+++ src/test/java/com/tmzkt/factorization/DivisibilityRuleTest.java
@@ -43,4 +43,39 @@ public class DivisibilityRuleTest {
     public void LargeEvenNumberDivisibleBy2() {
         assertTrue(divisibilityRule.divisibleBy2(BigInteger.valueOf(35487946948L)));
     }
+
+    @Test
+    public void ZeroDivisibleBy3() {
+        assertTrue(divisibilityRule.divisibleBy3(BigInteger.ZERO));
+    }
+
+    @Test
+    public void OneNotDivisibleBy3() {
+        assertFalse(divisibilityRule.divisibleBy3(BigInteger.ONE));
+    }
+
+    @Test
+    public void NineDivisibleBy3() {
+        assertTrue(divisibilityRule.divisibleBy3(BigInteger.valueOf(9)));
+    }
+
+    @Test
+    public void NegativeNumberNotDivisibleBy3() {
+        assertFalse(divisibilityRule.divisibleBy3(BigInteger.valueOf(-99811)));
+    }
+
+    @Test
+    public void NegativeNumberDivisibleBy3() {
+        assertTrue(divisibilityRule.divisibleBy3(BigInteger.valueOf(-37422)));
+    }
+
+    @Test
+    public void LargeNumberNotDivisibleBy3() {
+        assertFalse(divisibilityRule.divisibleBy3(BigInteger.valueOf(923759284721L)));
+    }
+
+    @Test
+    public void LargeNumberDivisibleBy3() {
+        assertTrue(divisibilityRule.divisibleBy3(BigInteger.valueOf(354879469482L)));
+    }
 }
