7b01fab67a9fcba693c128be76c1a215c4098490
Scott <ssk_8189@yahoo.com>
2017-02-22 19:43:42 -0500

    add divisibility rule for 3

--- src/main/java/com/tmzkt/factorization/DivisibilityRule.java
+++ src/main/java/com/tmzkt/factorization/DivisibilityRule.java
@@ -8,4 +8,16 @@ public class DivisibilityRule {
         int lastDigit = Character.getNumericValue(stringRepresentation.charAt(stringRepresentation.length() - 1));
         return lastDigit % 2 == 0;
     }
+
+    public boolean divisibleBy3(BigInteger n) {
+        String stringRepresentation = n.abs().toString();
+        while (stringRepresentation.length() > 1) {
+            long totalOfDigits = 0;
+            for (char c : stringRepresentation.toCharArray()) {
+                totalOfDigits += Character.getNumericValue(c);
+            }
+            stringRepresentation = String.valueOf(totalOfDigits);
+        }
+        return Integer.parseInt(stringRepresentation) % 3 == 0;
+    }
 }
